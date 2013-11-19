52717842c8e1e8f29bb1d463dec9b5df85580180
EncoderZero <intrepidhonor@gmail.com>
2013-11-19 15:20:33 -0500

    plans for bridging the two sides of project

--- src/MatchCardGame/CardGameEngine.java
+++ src/MatchCardGame/CardGameEngine.java
@@ -7,5 +7,10 @@ package MatchCardGame;
  *Handles back end of the card game programming.
  */
 public class CardGameEngine {
-
+//TODO get cards function, pass display string array with picture names. formmated by card_#suit
+	//TODO check if cards if first card is selected and second matches return string message value. Passed array numbers of card list to check
+	//TODO score display calculate new score and update score message string as string
+	//TODO create a function checkHighScore returns number 0 - 5 where 5 is not in top five.
+	//TODO create a function changeInitials (int arraylocation, string newName)
+	//TODO private addscore, public get score return int;
 }

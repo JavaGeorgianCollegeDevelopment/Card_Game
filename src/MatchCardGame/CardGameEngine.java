605ed610e949ea0b1b5eb572f51fc7c63546eea6
EncoderZero <intrepidhonor@gmail.com>
2013-11-19 14:34:24 -0500

    Create Assignment project file

---
+++ src/MatchCardGame/CardGameEngine.java
@@ -0,0 +1,11 @@
+package MatchCardGame;
+/**
+ * 
+ * @author Kevin Kan
+ *@version 1 
+ *@since November 19 2013
+ *Handles back end of the card game programming.
+ */
+public class CardGameEngine {
+
+}
---
+++ src/MatchCardGame/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Jean Luc and Kevin Kan
+ * @version 1
+ * @since November 19 2013
+ *Match Cardgame package.
+ */
+package MatchCardGame;

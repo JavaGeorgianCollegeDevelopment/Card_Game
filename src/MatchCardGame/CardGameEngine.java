eea3c3b636f756a742a0f5e2ace2649ff9f438b9
EncoderZero <intrepidhonor@gmail.com>
2013-11-21 11:57:34 -0500

    change comments for code

--- src/MatchCardGame/CardGameEngine.java
+++ src/MatchCardGame/CardGameEngine.java
@@ -7,10 +7,9 @@ package MatchCardGame;
  *Handles back end of the card game programming.
  */
 public class CardGameEngine {
-//TODO get cards function, pass display string array with picture names. formmated by card_#suit
-	//TODO check if cards if first card is selected and second matches return string message value. Passed array numbers of card list to check
-	//TODO score display calculate new score and update score message string as string
-	//TODO create a function checkHighScore returns number 0 - 5 where 5 is not in top five.
+//TODO function getCards (), return string array with picture names. formmated by card_#suit
+	//TODO fuction checkMatch(int arraylocation),check if cards if first card is selected and second matches return string message value. Passed array numbers of card list to check
+	//TODO function getScore (),return int score
+	//TODO create a function checkHighScore (), returns int 0 - 5 where 5 is not in top five.
 	//TODO create a function changeInitials (int arraylocation, string newName)
-	//TODO private addscore, public get score return int;
 }

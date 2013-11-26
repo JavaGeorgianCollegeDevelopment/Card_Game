349b3aeebd82479ea513bafe9d200cdc1698f581
EncoderZero <intrepidhonor@gmail.com>
2013-11-25 20:04:43 -0500

    added on read xml , finished bridging functions, added testing file for game engine

--- src/MatchCardGame/CardGameEngine.java
+++ src/MatchCardGame/CardGameEngine.java
@@ -1,42 +1,131 @@
 package MatchCardGame;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 /**
  * 
  * @author Kevin Kan
- *@version 1 
- *@since November 19 2013
+ *@version 1.1 
+ *@since November 25 2013
  *Handles back end of the card game programming.
  */
 public class CardGameEngine {
 	//var
 	private static final int CARDS_IN_DECK = 52;
 	private String[] fullCardList= new String[CARDS_IN_DECK];
-	private ArrayList<String> selectedCards;
+	private ArrayList<Integer> highScores=new ArrayList<Integer>();
+	private ArrayList<String> selectedCards=new ArrayList<String>(),highScoreName= new ArrayList<String>();
 	private String checkCard="";
 	private int score;
-	//defualt constructor
+
+	/**
+	 * default constructor that sets the full card list values and resest the score to zero
+	 */
 	CardGameEngine(){//add all possible card names
-		for (int i=1; i<14; i++){
-			fullCardList[i]="card_"+i+"s";
-			fullCardList[i]="card_"+i+"c";
-			fullCardList[i]="card_"+i+"d";
-			fullCardList[i]="card_"+i+"h";
+		for (int i=0; i<13; i++){
+			fullCardList[i]="card_"+(i+1)+"s";
+			fullCardList[i+13]="card_"+(i+1)+"c";
+			fullCardList[i+26]="card_"+(i+1)+"d";
+			fullCardList[i+39]="card_"+(i+1)+"h";
+			/*// debug full card list.
+			System.out.println(fullCardList[i]);
+			System.out.println(fullCardList[i+13]);
+			System.out.println(fullCardList[i+26]);
+			System.out.println(fullCardList[i+39]);*/
 		}
 		score=0;
+		//parse xml into java
+		//code from here is not entirely mine but I understand it and know what it does, from http://viralpatel.net/blogs/java-xml-xpath-tutorial-parse-xml/ 
+		try{
+			FileInputStream file = new FileInputStream(new File(getClass().getResource("resources/highScores/highScores.xml").getPath()));
+		    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		    DocumentBuilder builder =  builderFactory.newDocumentBuilder();
+		    Document xmlDocument = builder.parse(file);
+		    XPath xPath =  XPathFactory.newInstance().newXPath();
+		//end of borrowed code
+		    for(int t=1;t<6;t++){
+		    	String expressionNames = "/highScores/scores[@place='"+t+"']/name";
+		    	String expressionScore = "/highScores/scores[@place='"+t+"']/score";
+		    	try{
+		    		String scoreNameTemp=xPath.compile(expressionNames).evaluate(xmlDocument);
+		    		int scoreTemp=(Integer.parseInt(xPath.compile(expressionScore).evaluate(xmlDocument)));
+		    		if((scoreTemp==0)||(scoreNameTemp==null)){
+		    			highScores.add(1);
+			    		highScoreName.add("AAA");
+		    		}
+		    		else{
+		    			highScores.add(scoreTemp);
+			    		highScoreName.add(scoreNameTemp);
+		    		}
+		    		}
+		    	catch(NumberFormatException e){
+		    		System.out.println(e.getMessage());
+		    	}
+		    	catch(NullPointerException e){
+		    		System.out.println(e.getMessage());
+		    	}
+		    }
+		}
+		catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+	    }
+		catch (SAXException e) {
+			System.out.println(e.getMessage());
+	    } 
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+	    } 
+		catch (ParserConfigurationException e) {
+			System.out.println(e.getMessage());
+	    } 
+		catch (XPathExpressionException e) {
+			System.out.println(e.getMessage());
+		} 
 	}
+	
+	/**
+	 * reset's score to zero and picks new cards
+	 * @return  ArrayList<String> selectedCards is the array list that contains all of the cards that were selected for this game's file name.
+	 * returns an arraylist size 16 that has been shuffled and made up of 8 pairs.
+	 */
 	public ArrayList<String> getCards(){// function getCards (), return string array with picture names. formmated by card_#suit
-		final int NUMBER_OF_CARDS = 8;
+		final int NUMBER_OF_PAIRS = 8;
 		score=0;
+
+		long seed = System.nanoTime()*15215/6512;//get system time to use as a seed for random
 		selectedCards.clear();//clear previous set of selected cards.
 		String cardName;
-		Random randomIndex = new Random();
-		for (int j=0; j<NUMBER_OF_CARDS; j++){
-			cardName= fullCardList[randomIndex.nextInt(CARDS_IN_DECK-1)];
+		Collections.shuffle(Arrays.asList(fullCardList),new Random (seed));
+		for (int j=0; j<NUMBER_OF_PAIRS; j++){
+			cardName= fullCardList[j];
+			selectedCards.add(cardName);
 			selectedCards.add(cardName);
 		}
+		seed = System.nanoTime()*42/3;//re shuffle seed
+		Collections.shuffle(selectedCards,new Random (seed));//scramble the selectedCards array randomly.
 		return selectedCards;
 	}
+	
+	/**takes the first card value and saves it. Then checks the second card if they match. 
+	 * @param checkSelcetedCardArrayIndex is the integer index of the array
+	 * @return string message that ranges from empy in case of first card selected, Right or wrong depending if the card files matches.
+	 */
 	public String checkMatch(int checkSelcetedCardArrayIndex){//TODO fuction checkMatch(int arraylocation),check if cards if first card is selected and second matches return string message value. 
 		if(checkCard==""){
 			checkCard=selectedCards.get(checkSelcetedCardArrayIndex);
@@ -57,12 +146,46 @@ public class CardGameEngine {
 	private void addScore(int addValue){
 		score = score+addValue;
 	}
+	/**returns the score to the user
+	 * @return int score
+	 */
 	public int getScore(){//TODO function getScore (),return int score
 		return score;
 	}
+	/**This function checks to see if the user has toped the high score
+	 * @return y which is the index of highscores. 0 being first place and 4 being fifth place. Returns 5 if did not make it to highScores.
+	 * also automatically assigns associated score name with score as "AAA"
+	 */
 	public int checkHighScore(){//TODO create a function checkHighScore (), returns int 0 - 5 where 5 is not in top five.
-		return 0;
+		for(int y=0; y<5; y++){
+			if(this.getScore()>highScores.get(y)){
+				//replace the old score with new one and bump every on else down one
+				highScores.add(y,this.getScore());
+				highScoreName.add(y,"AAA");
+				return y;
+			}
+		}
+		return 5;//failing that not better than any other score return 5
 	}
 	
-	//TODO create a function changeInitials (int arraylocation, string newName)
+	/** Changes the highscore's name based on the place they ranked.
+	 * @param place integer that denotes where they ranked. 
+	 * @param newName string of new name they wish to change the score to.
+	 */
+	public void changeInitials(int place, String newName){//TODO create a function changeInitials (int arraylocation, string newName)
+		highScoreName.set(place,newName);
+	}
+	
+	/**Gets arraylist of highScorers Names
+	 * @return highScoresName ArrayList<String>
+	 */
+	public ArrayList<String> getHighScoreName(){
+		return highScoreName;
+	}
+	/**Gets array list of highScore scores
+	 * @return highScores ArrayList<Integer>
+	 */
+	public ArrayList<Integer> getHighScores(){
+		return highScores;
+	}
 }
---
+++ src/MatchCardGame/testGameEngine.java
@@ -0,0 +1,13 @@
+package MatchCardGame;
+
+public class testGameEngine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CardGameEngine cge = new CardGameEngine();
+		System.out.println(cge.getHighScores());
+		System.out.println(cge.getHighScoreName());
+		System.out.println(cge.getCards());
+	}
+
+}
